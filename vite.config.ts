f7db6e9ec4deb232c5e85bf41b60b673b2a72653
Dibyadyuti Das <dibyadyutidas0@gmail.com>
2025-06-15 21:33:23 +0530

    update Vite configuration for project setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/DibyadyutiDas/FocusBeats', // <-- important!
+  base: '/FocusBeats/', // ✅ ONLY the repo name with slashes
 });
